a552e06fab015b49d82fc8053d290a53c6284861
Anwar <anwardote@gmail.com>
2022-12-07 11:53:11 +0600

    fixing migration

--- database/migrations/2022_12_07_053617_add_bdthemes_account_id_field_to_users_table.php
+++ database/migrations/2022_12_07_053617_add_bdthemes_account_id_field_to_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,7 +13,9 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            if (!Schema::hasColumn('users', 'bdthemes_account_id')) {
+                $table->uuid('bdthemes_account_id')->index()->nullable();
+            }
         });
     }
 
@@ -25,8 +26,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        if (Schema::hasColumn('users', 'bdthemes_account_id')) {
+            Schema::dropColumns('users', 'bdthemes_account_id');
+        }
     }
 };
